b58cd18a319100f363439add075c8db8480b07c3
chrisf223 <chrisfranceschini23@gmail.com>
2025-03-10 15:11:28 -0400

    Exercise 5

---
+++ Exercise5/models/story.js
@@ -0,0 +1,22 @@
+const { DateTime } = require("luxon");
+const {v4: uuidv4} = require('uuid');
+const {ObjectId} = require('mongodb');
+
+//need a reference variable to the stories collection in mongodb
+let stories;
+exports.getCollection = db=>{
+    stories = db.collection('stories');
+}
+
+exports.find = () => stories.find().toArray();
+
+exports.findById = id => stories.findOne({_id: new ObjectId(`${id}`)});
+
+exports.save = story => stories.insertOne(story);
+    
+exports.updateById = (id, newStory) => stories.updateOne({_id: new ObjectId(`${id}`)}, 
+                        {$set:{title: newStory.title, content: newStory.content}});
+    
+exports.deleteById = (id) => stories.deleteOne({_id: new ObjectId(`${id}`)}); 
+    
+                        
